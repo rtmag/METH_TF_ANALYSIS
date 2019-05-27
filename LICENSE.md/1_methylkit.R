8ea7b3be7cb41338ce1c916c0c9060f8f03ee610
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-27 22:23:27 +0800

    Create 1_methylkit.R

---
+++ LICENSE.md/1_methylkit.R
@@ -0,0 +1,25 @@
+library(methylKit)
+                
+file.list <- list.files("/home/rtm/methmotif_cov/WGBS_MethMotif/",recursive=TRUE,full.names = TRUE)
+file.list <- as.list(file.list)
+
+cells <- gsub("\\/home\\/rtm\\/methmotif_cov\\/WGBS_MethMotif\\/\\/","",file.list)
+cells <- gsub("\\/.+","",cells,perl=TRUE)
+
+cell_sample_number <- as.numeric(factor(cells))
+
+cells <- make.unique(cells)
+cells <- gsub("\\.1","\\.2",cells)
+cells[grep("\\.2",cells)-1] <- paste(cells[grep("\\.2",cells)-1],".1",sep="")
+
+myobj=methRead(file.list,
+           sample.id = as.list(cells),
+           assembly = "hg38",
+           treatment = cell_sample_number,
+           context = "CpG",
+           pipeline = "bismarkCoverage",
+           header = FALSE,
+           mincov=5)
+           
+           
+           
