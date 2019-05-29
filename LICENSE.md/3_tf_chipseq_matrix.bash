a4d830348a8bf53d62b94933de738dda1154ac73
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-30 01:57:02 +0800

    Create 3_tf_chipseq_matrix.bash

---
+++ LICENSE.md/3_tf_chipseq_matrix.bash
@@ -0,0 +1,20 @@
+/home/rtm/methmotif_cov/tfregulomeR/peaks_motif/
+
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*A549*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*GM12878*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*H1-hESC*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*H9*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*HCT116*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*HEK293*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*HEK293T*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*HeLa-S3*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*HepG2*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*HUES64*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*IMR-90*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*K562*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*LNCaP*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*MCF-7*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*SK-N-SH*
+cat /home/rtm/methmotif_cov/tfregulomeR/peaks_motif/MM1*SNU398*
+
+
