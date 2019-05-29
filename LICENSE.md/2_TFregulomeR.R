9f10b45b674d3af05b47db950e93b38189ae8473
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-05-29 11:51:36 +0800

    Update 2_TFregulomeR.R

--- LICENSE.md/2_TFregulomeR.R
+++ LICENSE.md/2_TFregulomeR.R
@@ -1,2 +1,13 @@
 library(TFregulomeR)
 all_record <- TFBSBrowser()
+
+# download all peaks with motif
+for (i in all_record$ID){
+peak_i = loadPeaks(id=i, includeMotifOnly=T)
+write.table(peak_i, paste0(i,'_peaks_with_motif.txt'), sep='\t', col.names=F, row.names=F, quote=F)}
+
+# Downlload all peaks
+for (i in all_record$ID){
+peak_i = loadPeaks(id=i, includeMotifOnly=F)
+write.table(peak_i, paste0(i,'_peaks_all.txt'), sep='\t', col.names=F, row.names=F, quote=F)}
+
