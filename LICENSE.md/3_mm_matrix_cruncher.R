4d97aa1d34a32f45343cc7c8450e8f4d964774da
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-06-12 16:26:32 +0800

    Update 3_mm_matrix_cruncher.R

--- LICENSE.md/3_mm_matrix_cruncher.R
+++ LICENSE.md/3_mm_matrix_cruncher.R
@@ -1,6 +1,7 @@
 
 mm <- read.table("mm_tf_matrix.bed",header=TRUE)
-coord < - mm[,1:3]
+coord <- mm[,1:3]
 
 # system("cut -f1,2,3 mm_tf_matrix.bed|bedtools merge -i - > mm_tf_matrix_merged.bed ")
-merged <- read.table(mm_tf_matrix_merged.bed,sep="\t",header=FALSE)
+merged <- read.table("mm_tf_matrix_merged.bed",sep="\t",header=FALSE)
+
