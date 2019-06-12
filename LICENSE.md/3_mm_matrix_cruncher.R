6c26616d0c19775c1577e35d2fd98dad5c7ccd6c
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-06-12 16:55:38 +0800

    Update 3_mm_matrix_cruncher.R

--- LICENSE.md/3_mm_matrix_cruncher.R
+++ LICENSE.md/3_mm_matrix_cruncher.R
@@ -4,4 +4,11 @@ coord <- mm[,1:3]
 
 # system("cut -f1,2,3 mm_tf_matrix.bed|bedtools merge -i - > mm_tf_matrix_merged.bed ")
 merged <- read.table("mm_tf_matrix_merged.bed",sep="\t",header=FALSE)
+colnames(merged) <- c("chr","start","end")
+
+library(rtracklayer)
+coord.gr <- makeGRangesFromDataFrame(coord) 
+merged.gr <- makeGRangesFromDataFrame(merged) 
+hits <- findOverlaps(coord.gr, merged.gr)
+
 
