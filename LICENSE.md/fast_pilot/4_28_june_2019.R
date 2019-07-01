928a119ee76e64db379cd5d3064d0d80ed766670
Roberto Tirado Magallanes <rtmag4@gmail.com>
2019-07-01 21:32:34 +0800

    Create 4_28_june_2019.R

---
+++ LICENSE.md/fast_pilot/4_28_june_2019.R
@@ -0,0 +1,42 @@
+library(data.table)
+library(rtracklayer)
+options(scipen=999)
+############
+# WGBS bed cov file
+file.list <- c("/home/rtm/methmotif_cov/WGBS_MethMotif/A549/A549.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/GM12878/GM12878.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/H1-hESC/H1-hESC.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/H9/H9.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/HCT116/combine_hct116_WGBS_1_bismark_bt2_pe.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/HEK293/GSM1254259_r1_bismark_bt2_pe.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/HEK293T/HEK239T_WGBS_r1_bismark_bt2_pe.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/HeLa-S3/SRR3574510_1_val_1_bismark_bt2_pe.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/HepG2/HepG2.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/HUES64/HUES64.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/IMR-90/ENCFF937OSM.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/K562/K562.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/LNCaP/SRR5219980_1_bismark_bt2_pe.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/MCF-7/SRR1169729_1_bismark_bt2_pe.bismark.destranded.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/SK-N-SH/SK-N-SH.bismark.destranded.pooled.cov",
+               "/home/rtm/methmotif_cov/WGBS_MethMotif/SNU398/SNU398_WGBS_r1_bismark_bt2_pe.bismark.destranded.cov")
+
+wgbs = list()
+wgbs.gr = list()
+
+for(i in 1:length(file.list) ){
+  cell.name <- gsub("/home/rtm/methmotif_cov/WGBS_MethMotif/","",file.list[i])
+  cell.name <- gsub("\\/.+cov","",cell.name,perl=TRUE)
+  print(cell.name)
+  covfile <- fread( file.list[i],
+                  sep="\t",
+                  data.table=TRUE, header=FALSE)
+  covfile[,2] = covfile[,2]-1
+  wgbs[[cell.name]] <- covfile
+  
+  wgbs_df <- as.data.frame(covfile[,1:3])
+  colnames(wgbs_df) <- c("chr","start","end")
+  wgbs_i.gr <- makeGRangesFromDataFrame(wgbs_df)
+  wgbs.gr[[cell.name]] <- wgbs_i.gr
+
+  }
+
